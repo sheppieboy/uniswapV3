f8962e52ecbc2acaa608fbede75ee29c6610d697
Luke Sheppard <sheppiegit@gmail.com>
2024-07-19 16:19:55 -0400

    further price calculation of uniswap v3 with price to square root P, which uniswap V3 stores.  This uses 2**96 to simulate precision, and multiplies first before converting to an int to ensure as little precision loss as possible.

--- univ3-math.py
+++ univ3-math.py
@@ -5,4 +5,12 @@ def price_to_tick(p):
     r = math.floor(math.log(p, 1.0001))
     print(r)
 
-price_to_tick(5000);
+#uniswap v3 uses Q64.96 number to store sqrt P, this is a fixed point number that has 64 buts for the integer part and 96 bits for the fractional part
+q96 = 2 **96
+
+def price_to_sqrtP(p):
+    r = int(math.sqrt(p) * q96)
+    print(r)
+
+price_to_tick(5000)
+price_to_sqrtP(5000)
