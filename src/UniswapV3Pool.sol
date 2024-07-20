cf0d974d0196322911f8b211cc5f95ac2ad6cdba
Luke Sheppard <sheppiegit@gmail.com>
2024-07-19 22:46:49 -0400

    added the first part of the UniswapV3Pool mint function, right now it returns hardcoded values, updates ticks and positions, and liquidity.

--- src/UniswapV3Pool.sol
+++ src/UniswapV3Pool.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.14;
 import {Position} from "./lib/Position.sol";
 import {Tick} from "./lib/Tick.sol";
 
+error InvalidTickRange();
+error ZeroLiquidity();
 
 contract UniswapV3Pool {
     using Tick for mapping(int24 => Tick.Info);
@@ -40,4 +42,33 @@ contract UniswapV3Pool {
 
         slot0 = Slot0({sqrtPriceX96: sqrtPriceX96, tick: tick});
     }
+
+    //take owners address, to track the owner of liquidity
+    //upper and lower ticks, to set the bounds of a price range
+    //the amount of liquidity we want to provide
+    function mint(address owner, int24 lowerTick, int24 upperTick, uint128 amount) external returns (uint256 amount0, uint256 amount1) {
+        
+        /* outline of how minting works:
+        1. a user specifies a price range and an amount of liquidity
+        2. the contract updates the ticks and positions mappings
+        3. the contract calculates token amounts the user must send
+        4. the contract takes tokens from the user and verifies that the correct amounts were set
+        */
+
+        if (lowerTick >= upperTick || lowerTick < MIN_TICK || upperTick > MAX_TICK) revert InvalidTickRange();
+
+        if (amount == 0) revert ZeroLiquidity();
+
+        ticks.update(lowerTick, amount);
+        ticks.update(upperTick, amount); 
+        Position.Info storage position = positions.get(owner, lowerTick, upperTick);
+        position.update(amount);
+
+        //HARDCODED FOR NOW
+        amount0 = 0.998976618347425280 ether;
+        amount1 = 5000 ether;
+
+        liquidity += uint256(amount);
+    }
+
 }
--- src/lib/Tick.sol
+++ src/lib/Tick.sol
@@ -6,4 +6,16 @@ library Tick {
         bool initialized;
         uint128 liquidity;
     }
+
+    function update(mapping(int24 => Tick.Info) storage self, int24 tick, uint128 liquidityDelta) internal {
+        Tick.Info storage tickInfo = self[tick];
+        uint128 liquidityBefore = tickInfo.liquidity;
+        uint128 liquidityAfter = liquidityBefore + liquidityDelta;
+
+        if (liquidityBefore == 0) {
+            tickInfo.initialized = true;
+        }
+
+        tickInfo.liquidity = liquidityAfter;
+    }
 }
