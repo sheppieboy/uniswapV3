8499221d2a9b3fff739df0e041406e6be2f56f65
Luke Sheppard <sheppiegit@gmail.com>
2024-07-19 23:32:04 -0400

    added mock ERC20 for testing the UniswapV3Pool contract.

--- test/UniswapV3Pool.t.sol
+++ test/UniswapV3Pool.t.sol
@@ -2,9 +2,16 @@
 pragma solidity ^0.8.14;
 
 import {Test, console} from "forge-std/Test.sol";
+import {ERC20Mintable} from "./ERC20Mintable.sol";
 
 contract UniswapV3PoolTest is Test {
-    function setUp() public {}
+    ERC20Mintable token0;
+    ERC20Mintable token1;
+
+    function setUp() public {
+        token0 = new ERC20Mintable("Ether", "ETH", 18);
+        token1 = new ERC20Mintable("USDC", "USDC", 18);
+    }
 
     function testExample() public pure{
         assertTrue(true);
